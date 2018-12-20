ce205f9439106df093a13ab7ba5722f36c8266cc
Airaken <32349665+Airaken@users.noreply.github.com>
2018-12-20 11:45:50 -0500

    actualizacion del curso

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 require('./config/config');
+
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
@@ -9,32 +11,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensjae: 'el nombre es necesario'
-        })
-    } else {
-        res.json({ persona: body });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos Online');
 });
 
 app.listen(process.env.PORT, () => {
