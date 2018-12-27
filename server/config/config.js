30392f131ded9b6f909f9702d91e423ccdcef5cc
Airaken <32349665+Airaken@users.noreply.github.com>
2018-12-27 10:37:52 -0500

    google signin

--- server/config/config.js
+++ server/config/config.js
@@ -35,3 +35,9 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+//=====
+// Google Client ID
+//=====
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '750199709188-p70mnj4482sjsgh1kin9kn9v9hkudcuf.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -3,13 +3,18 @@ require('./config/config');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const path = require('path');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// habilitar carpeta public
+
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 //Confuguracioin global de rutas
 app.use(require('./routes/index'));
